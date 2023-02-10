74ec0e5de78b184f22fe0180388fb30b32a1f0e1
MakoreAkaRaku <marcromancolom@gmail.com>
2023-02-10 04:25:26 +0100

    Directory check on '/Songs' folder done. Now must contain at least two folders (.gitignore included) to start with.

--- src/SongVisualizer.cpp
+++ src/SongVisualizer.cpp
@@ -1,7 +1,30 @@
+#define _ALLOW_RTCc_IN_STL // Adding definition to cancel RTC on filesystem lib
 #include "SongVisualizer.h"
+#include <filesystem>
+#include <iostream>
+using namespace std::filesystem;
 
 int main(int n, char **args) {
-	wchar_t h = wchar_t("Hello world");
-	Song *s = new Song(&h);
-	delete s;
+	std::error_code err;
+	path c_path = current_path();
+	c_path.append(DIR_NAME);
+	if (!exists(c_path)){
+		if (!create_directory(c_path,err)) {
+			std::cerr<< err.message();
+			return 1;
+		}
+	}
+	directory_iterator dir_it = directory_iterator(c_path);
+	directory_iterator itBeg = begin(dir_it);
+	directory_iterator itEnd = end(dir_it);
+	itBeg++;
+	while (is_empty(c_path)|| itBeg == itEnd) {
+		std::cerr << "Directory has no songs, please put at least a song\nPress 'Enter' key to continue\n";
+		std::cin.get() >> n;
+		dir_it = directory_iterator(c_path);
+		itBeg = begin(dir_it);
+		itEnd = end(dir_it);
+		itBeg++;
+	}
+	std::cout << c_path;
 }
--- src/SongVisualizer.h
+++ src/SongVisualizer.h
@@ -1,2 +1,3 @@
 #pragma once
 #include "Song.h"
+const char DIR_NAME[] = "Songs";
