74e1ed3069aca3079cd5a5cfb39190ae535fd8ef
MakoreAkaRaku <marcromancolom@gmail.com>
2023-02-25 17:00:03 +0100

    Changed types for wav_header struct, also implemented out() function.

--- includes/AudioManager.h
+++ includes/AudioManager.h
@@ -23,24 +23,27 @@ using namespace std;
 * [<PLAYLIST - CK>]			// PLAYLIST
 * [<ASSOC - DATA - LIST>]	// ASSOCIATED DATA LIST
 * <WAVE - DATA>)			// WAVE DATA
+* 
+*  <<<<<< IMPORTANT: due to portability for the platform, instead of using long and short, i'm using uintxx >>>>>>
 */
 struct WAV_Header {
 			/* RIFF Chunk Descriptor */
 	char                RIFF[4];        // RIFF Header (dunno )
-	unsigned long       ChunkSize;      // RIFF Chunk Size  
+	uint32_t			ChunkSize;      // RIFF Chunk Size  
 	char                WAVE[4];        // WAVE Header      
 			/* "fmt" sub-chunk */
 	char                fmt[4];         // FMT header       
-	unsigned long       Subchunk1Size;  // Size of the fmt chunk                                
-	unsigned short      AudioFormat;    // Audio format 1=PCM,6=mulaw,7=alaw, 257=IBM Mu-Law, 258=IBM A-Law, 259=ADPCM 
-	unsigned short      NumOfChan;      // Number of channels 1=Mono 2=Sterio                   
-	unsigned long       SamplesPerSec;  // Sampling Frequency in Hz                             
-	unsigned long       bytesPerSec;    // bytes per second 
-	unsigned short      blockAlign;     // 2=16-bit mono, 4=16-bit stereo 
-	unsigned short      bitsPerSample;  // Number of bits per sample      
+	uint32_t			Subchunk1Size;  // Size of the fmt chunk                                
+	uint16_t			AudioFormat;    // Audio format 1=PCM,6=mulaw,7=alaw, 257=IBM Mu-Law, 258=IBM A-Law, 259=ADPCM 
+	uint16_t			NumOfChan;      // Number of channels 1=Mono 2=Sterio                   
+	uint32_t			SamplesPerSec;  // Sampling Frequency in Hz                             
+	uint32_t			bytesPerSec;    // bytes per second 
+	uint16_t			blockAlign;     // 2=16-bit mono, 4=16-bit stereo 
+	uint16_t			bitsPerSample;  // Number of bits per sample      
 			/* "data" sub-chunk */
 	char                Subchunk2ID[4]; // "data"  string   
-	unsigned long       Subchunk2Size;  // Sampled data length    
+	uint32_t			Subchunk2Size;  // Sampled data length    
+	const void out(); //Prints out the actual value of the whole struct.
 
 };
 
